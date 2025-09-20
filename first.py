470f9373b62872aee854f60575c170b561252f69
ALNoman78 <abdullaalno078@gmail.com>
2025-09-20 20:57:22 +0600

    python sec draft

--- first.py
+++ first.py
@@ -1,1 +1,6 @@
+#this is just a basic concept
 print("Hello World !")
+a = 20;
+b = 30;
+print(a+b)  
+
---
+++ second.py
@@ -0,0 +1,10 @@
+a, b,c = "Abdulla" , "Al", "Noman"
+print(a,b,c)
+print(type(a))
+
+JobTitle = ["Frontend Development" , "AI/ML" , "Backend Development" , "DSA"]
+x, y, z , i = JobTitle
+print(x)
+print(y)
+print(z)
+print(i)
